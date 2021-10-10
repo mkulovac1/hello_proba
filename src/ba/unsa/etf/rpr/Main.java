977bf452a2e1f1132201d357097955bd9ddc2f2a
Merim <mkulovac1@etf.unsa.ba>
2021-10-10 21:57:25 +0200

    Dodani novi printfovi

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("Zdravo raja");
         System.out.println("Sta se radi?");
+        System.out.println("Kako ste?");
     }
 }
